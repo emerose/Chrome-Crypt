b42fb07a73e82e7847daeee3f50b31f6cf7465e1
Sam Quigley <sq@wesabe.com>
2010-01-22 18:38:15 -0800

    WIP - add dummy functions

--- ChromeCrypt.c
+++ ChromeCrypt.c
@@ -8,7 +8,7 @@
 
 #include "ChromeCrypt.h"
 
-static NPObject scriptObject;
+static NPNetscapeFuncs *browser; // we store the browser function table here for future use
 
 /**
  * Called once (before NP_Initialize?) in order to set up pointers required for
@@ -30,6 +30,7 @@ NPError NP_GetEntryPoints(NPPluginFuncs* pluginFuncs)
  */
 NPError NP_Initialize(NPNetscapeFuncs* browserFuncs)
 {  
+  browser = browserFuncs;
   return NPERR_NO_ERROR;
 }
 
@@ -64,10 +65,8 @@ NPError NPP_GetValue(NPP instance,         // the instance
       return NPERR_NO_ERROR;
 
     case NPPVpluginScriptableNPObject:// Scriptable plugin interface (for accessing from javascript)
-/*      *((NPObject **)value) = &scriptObject;
+      *((NPObject **)value) = newScriptObject(instance);
       return NPERR_NO_ERROR;
-*/
-      return NPERR_GENERIC_ERROR;
 
     case NPPVpluginWindowBool:
       *((bool **)value) = FALSE;
@@ -93,3 +92,70 @@ void NP_Shutdown(void)
 {
   return;
 }
+
+/**********************/
+
+/**
+ * creates a new NPObject for use in scripting
+ */
+NPObject *newScriptObject(NPP instance)
+{
+    // first, we allocate and zero an NPClass struct
+  NPClass *class = malloc(sizeof(NPClass));
+  memset(class, 0, sizeof(NPClass));
+  
+    // now, we fill it in with the right function pointers
+  class->structVersion  = NP_CLASS_STRUCT_VERSION;
+  class->hasMethod      = scriptHasMethod;
+  class->invoke         = scriptInvokeMethod;
+  class->invokeDefault  = scriptInvokeDefaultMethod;
+  class->hasProperty    = scriptHasProperty;
+  class->getProperty    = scriptGetProperty;
+  class->setProperty    = scriptSetProperty;
+  class->removeProperty = scriptRemoveProperty;
+  
+  return browser->createobject(instance, class);
+}
+
+bool scriptHasMethod(NPObject *npobj, NPIdentifier name)
+{
+  return false;
+}
+
+bool scriptInvokeMethod(NPObject *npobj,
+                        NPIdentifier name,
+                        const NPVariant *args,
+                        uint32_t argCount,
+                        NPVariant *result)
+{
+  return false;
+}
+
+bool scriptInvokeDefaultMethod(NPObject *npobj,
+                               const NPVariant *args,
+                               uint32_t argCount,
+                               NPVariant *result)
+{
+  return false;
+}
+
+bool scriptHasProperty(NPObject *npobj, NPIdentifier name)
+{
+  return false;
+}
+
+bool scriptGetProperty(NPObject *npobj, NPIdentifier name, NPVariant *result)
+{
+  return false;
+}
+
+bool scriptSetProperty(NPObject *npobj, NPIdentifier name, const NPVariant *value)
+{
+  return false;
+}
+
+bool scriptRemoveProperty(NPObject *npobj, NPIdentifier name)
+{
+  return false;
+}
+
--- ChromeCrypt.h
+++ ChromeCrypt.h
@@ -7,6 +7,7 @@
  */
 
 #include "include/plugin/npapi.h"
+#include "include/plugin/npruntime.h"
 #include "include/plugin/npfunctions.h"
 
 #define PLUGIN_NAME "ChromeCrypt"
@@ -18,3 +19,20 @@ NPError NPP_New(NPMIMEType pluginType, NPP instance, uint16_t mode, int16_t argc
 NPError NPP_GetValue(NPP instance, NPPVariable variable, void *value);
 NPError NPP_Destroy(NPP instance, NPSavedData** save);
 void    NP_Shutdown(void);
+
+
+NPObject *newScriptObject(NPP instance);
+bool scriptHasMethod(NPObject *npobj, NPIdentifier name);
+bool scriptInvokeMethod(NPObject *npobj,
+                        NPIdentifier name,
+                        const NPVariant *args,
+                        uint32_t argCount,
+                        NPVariant *result);
+bool scriptInvokeDefaultMethod(NPObject *npobj,
+                               const NPVariant *args,
+                               uint32_t argCount,
+                               NPVariant *result);
+bool scriptHasProperty(NPObject *npobj, NPIdentifier name);
+bool scriptGetProperty(NPObject *npobj, NPIdentifier name, NPVariant *result);
+bool scriptSetProperty(NPObject *npobj, NPIdentifier name, const NPVariant *value);
+bool scriptRemoveProperty(NPObject *npobj, NPIdentifier name);
